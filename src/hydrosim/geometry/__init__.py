30c4ef4af6935622d42acf06da5528769ec375bd
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-08-27 14:59:49 -0300

    Export rotation utilities for issue #3

--- src/hydrosim/geometry/__init__.py
+++ src/hydrosim/geometry/__init__.py
@@ -1,5 +1,21 @@
 """Geometry primitives for HydroSIM."""
 
 from .models import Attitude, Pose, Vector3
+from .rotations import (
+    rotate_vector,
+    rotation_matrix_from_rpy,
+    rotation_x,
+    rotation_y,
+    rotation_z,
+)
 
-__all__ = ["Attitude", "Pose", "Vector3"]
+__all__ = [
+    "Attitude",
+    "Pose",
+    "Vector3",
+    "rotate_vector",
+    "rotation_matrix_from_rpy",
+    "rotation_x",
+    "rotation_y",
+    "rotation_z",
+]
