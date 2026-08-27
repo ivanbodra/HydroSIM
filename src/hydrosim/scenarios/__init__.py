959d50f4201a6d81fc6608bf12d9873643a05a9d
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-08-27 19:12:32 -0300

    Add scenario package exports

---
+++ src/hydrosim/scenarios/__init__.py
@@ -0,0 +1,17 @@
+"""Pedagogical HydroSIM scenarios."""
+
+from .roll_offset import (
+    RollOffsetResult,
+    RollOffsetScenarioConfig,
+    RollOffsetSummary,
+    load_roll_offset_scenario,
+    run_roll_offset_scenario,
+)
+
+__all__ = [
+    "RollOffsetResult",
+    "RollOffsetScenarioConfig",
+    "RollOffsetSummary",
+    "load_roll_offset_scenario",
+    "run_roll_offset_scenario",
+]
