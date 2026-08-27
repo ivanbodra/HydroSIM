a6f5e23699842e02d9594cadd9ee65679375b035
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-08-27 13:26:00 -0300

    Expose core geometry models at package level

--- src/hydrosim/__init__.py
+++ src/hydrosim/__init__.py
@@ -1,3 +1,7 @@
 """HydroSIM scientific and didactic hydrographic surveying simulator."""
 
+from .geometry import Attitude, Pose, Vector3
+
 __version__ = "0.1.0"
+
+__all__ = ["Attitude", "Pose", "Vector3"]
