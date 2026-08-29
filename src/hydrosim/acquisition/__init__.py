0118b9b79793506eac0d0de3cef990164498f61d
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-08-28 21:53:51 -0300

    Expose receive-array truth reception API

--- src/hydrosim/acquisition/__init__.py
+++ src/hydrosim/acquisition/__init__.py
@@ -2,6 +2,11 @@
 
 from .generation import generate_acquisition_sequence
 from .models import AcquisitionPing, AcquisitionSequence, PingSchedule
+from .reception import (
+    ArrayElementTruthArrival,
+    ArrayTruthReception,
+    simulate_truth_array_reception,
+)
 from .returns import (
     BeamTruthReturn,
     ConstantSoundSpeedPropagation,
@@ -11,9 +16,12 @@ from .returns import (
 __all__ = [
     "AcquisitionPing",
     "AcquisitionSequence",
+    "ArrayElementTruthArrival",
+    "ArrayTruthReception",
     "BeamTruthReturn",
     "ConstantSoundSpeedPropagation",
     "PingSchedule",
     "generate_acquisition_sequence",
+    "simulate_truth_array_reception",
     "simulate_truth_beam_return",
 ]
