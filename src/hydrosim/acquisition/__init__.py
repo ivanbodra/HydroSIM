d1eeac2780ce9bf7c4e4613d0f12b86c309fb1af
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-08-28 21:33:14 -0300

    Expose beam return propagation API

--- src/hydrosim/acquisition/__init__.py
+++ src/hydrosim/acquisition/__init__.py
@@ -2,10 +2,18 @@
 
 from .generation import generate_acquisition_sequence
 from .models import AcquisitionPing, AcquisitionSequence, PingSchedule
+from .returns import (
+    BeamTruthReturn,
+    ConstantSoundSpeedPropagation,
+    simulate_truth_beam_return,
+)
 
 __all__ = [
     "AcquisitionPing",
     "AcquisitionSequence",
+    "BeamTruthReturn",
+    "ConstantSoundSpeedPropagation",
     "PingSchedule",
     "generate_acquisition_sequence",
+    "simulate_truth_beam_return",
 ]
