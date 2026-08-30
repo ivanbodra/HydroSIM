36a03f00a61c84be052a56dddb3fa2c4167310ca
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-08-30 15:13:27 -0300

    Export layered SVP explorer API

--- src/hydrosim/visualization/__init__.py
+++ src/hydrosim/visualization/__init__.py
@@ -5,6 +5,11 @@ does not require optional plotting dependencies; plotting libraries are imported
 only inside renderer functions.
 """
 
+from .layered_svp_explorer import (
+    LayeredSvpExplorerBeam,
+    LayeredSvpExplorerSnapshot,
+    prepare_layered_svp_explorer_snapshot,
+)
 from .mills_cross_pattern import (
     MillsCrossPatternPanels,
     plot_mills_cross_pattern_panels,
@@ -12,7 +17,10 @@ from .mills_cross_pattern import (
 )
 
 __all__ = [
+    "LayeredSvpExplorerBeam",
+    "LayeredSvpExplorerSnapshot",
     "MillsCrossPatternPanels",
     "plot_mills_cross_pattern_panels",
+    "prepare_layered_svp_explorer_snapshot",
     "prepare_mills_cross_pattern_panels",
 ]
