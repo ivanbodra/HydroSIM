76c87a2eef2489a210f5f0f4af8802d4708750bb
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-08-31 21:37:08 -0300

    concept: expose signal lab prototype route

--- concepts/pedagogical-simulator/src/main.tsx
+++ concepts/pedagogical-simulator/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import SignalLab from './SignalLab';
 import './styles.css';
 
+function ConceptRuntime() {
+  const [view, setView] = useState(location.hash === '#signal-lab' ? 'signal' : 'map');
+  if (view === 'signal') return <SignalLab onBack={() => { location.hash = ''; setView('map'); }} />;
+  return <div onDoubleClick={() => { location.hash = 'signal-lab'; setView('signal'); }}><App /></div>;
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode><ConceptRuntime /></React.StrictMode>,
 );
