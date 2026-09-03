e81f8b1c3717e49283e63575c00531ab1d3c1f9e
ivanbodra <135233331+ivanbodra@users.noreply.github.com>
2026-09-03 17:40:24 -0300

    Export uncertainty and verification API

--- src/hydrosim/integration/__init__.py
+++ src/hydrosim/integration/__init__.py
@@ -15,14 +15,34 @@ from .risc_maingot import (
     maingot_surface_sound_speed,
     maingot_surface_sound_speed_steering_angle,
 )
+from .uncertainty import (
+    DerivedSemanticState,
+    InputSemanticState,
+    PropagatedUncertainty,
+    TruthErrorVector,
+    UncertainInputSet,
+    VerificationResidual,
+    compute_truth_error,
+    compute_verification_residual,
+    propagate_uncertainty,
+)
 
 __all__ = [
     "AdjustedMotionState",
+    "DerivedSemanticState",
+    "InputSemanticState",
+    "PropagatedUncertainty",
+    "TruthErrorVector",
+    "UncertainInputSet",
+    "VerificationResidual",
     "apply_maingot_motion_errors",
+    "compute_truth_error",
+    "compute_verification_residual",
     "configured_lever_arm_from_maingot_error",
     "hydrosim_lever_arm_error_from_maingot",
     "hydrosim_sss_error_from_maingot",
     "maingot_latency_adjusted",
     "maingot_surface_sound_speed",
     "maingot_surface_sound_speed_steering_angle",
+    "propagate_uncertainty",
 ]
